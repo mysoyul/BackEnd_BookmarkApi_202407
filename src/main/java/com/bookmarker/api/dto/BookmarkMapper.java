309700cfc18bdf7e35f72229a7e0575f648f6ccb
mysoyul <vega2k_@naver.com>
2024-07-23 10:18:28 +0900

    Bookmark API first commit

---
+++ src/main/java/com/bookmarker/api/dto/BookmarkMapper.java
@@ -0,0 +1,18 @@
+package com.bookmarker.api.dto;
+
+import com.bookmarker.api.domain.Bookmark;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookmarkMapper {
+
+    //Entity -> DTO
+    public BookmarkDTO toDTO(Bookmark bookmark) {
+        return new BookmarkDTO(
+            bookmark.getId(),
+            bookmark.getTitle(),
+            bookmark.getUrl(),
+            bookmark.getCreatedAt()
+        );
+    }
+}
